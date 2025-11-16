1821643945331e2caf0615992f70ba17416a5c88
Aung Min Ko <aungminko951357.com>
2025-11-16 23:02:59 +0630

    feat:authentication_api_add

---
+++ app/validators/auth.ts
@@ -0,0 +1,16 @@
+import vine from '@vinejs/vine'
+
+export const registerValidator = vine.compile(
+  vine.object({
+    name: vine.string().trim(),
+    email: vine.string().trim().email(),
+    password: vine.string().trim().minLength(8),
+  })
+)
+
+export const loginValidator = vine.compile(
+  vine.object({
+    email: vine.string().trim().email(),
+    password: vine.string().trim().minLength(8),
+  })
+)
--- start/routes.ts
+++ start/routes.ts
@@ -8,9 +8,16 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
+
+const AuthController = () => import('#controllers/auth_controller')
 
 router.get('/', async () => {
   return {
     hello: 'world',
   }
 })
+
+router.post('/v1/register', [AuthController, 'register'])
+router.post('/v1/login', [AuthController, 'login'])
+router.post('/v1/logout', [AuthController, 'logout']).use(middleware.auth())
